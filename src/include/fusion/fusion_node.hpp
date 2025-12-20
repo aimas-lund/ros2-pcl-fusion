50968beb6dc60079f206264fd871606fb4d072f3
Aimas Lund <aimaslund@proton.me>
2025-12-20 19:53:36 +0100

    Added approx sync

--- src/include/fusion/fusion_node.hpp
+++ src/include/fusion/fusion_node.hpp
@@ -1,7 +1,13 @@
-#ifndef FUSION__FUSION_NODE_HPP_
-#define FUSION__FUSION_NODE_HPP_
+#pragma once
 
 #include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/string.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
+#include <memory>
 
 namespace fusion
 {
@@ -12,11 +18,17 @@ public:
   explicit FusionNode(const rclcpp::NodeOptions & options = rclcpp::NodeOptions());
 
 private:
-  void on_timer();
+  void sync_callback(
+    const sensor_msgs::msg::PointCloud2::ConstSharedPtr & a,
+    const sensor_msgs::msg::PointCloud2::ConstSharedPtr & b);
 
-  rclcpp::TimerBase::SharedPtr timer_;
+  message_filters::Subscriber<sensor_msgs::msg::PointCloud2> cloudA_;
+  message_filters::Subscriber<sensor_msgs::msg::PointCloud2> cloudB_;
+  std::shared_ptr<message_filters::Synchronizer<
+    message_filters::sync_policies::ApproximateTime<
+      sensor_msgs::msg::PointCloud2,
+      sensor_msgs::msg::PointCloud2>>> sync_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_;
 };
 
 }  // namespace fusion
-
-#endif  // FUSION__FUSION_NODE_HPP_
--- src/src/fusion_node.cpp
+++ src/src/fusion_node.cpp
@@ -3,21 +3,50 @@
 #include <chrono>
 #include <functional>
 
+
 namespace fusion
 {
-
-   using namespace std::chrono_literals;
 
 FusionNode::FusionNode(const rclcpp::NodeOptions & options)
 : rclcpp::Node("fusion_node", options)
 {
-  auto period = std::chrono::milliseconds{1000};
-  timer_ = this->create_wall_timer(period, std::bind(&FusionNode::on_timer, this));
+  auto qos = rclcpp::SensorDataQoS();
+  const uint32_t queue_size = 10;
+  const double slop = 0.1;
+
+  cloudA_.subscribe(this, "/cloudA", qos.get_rmw_qos_profile());
+  cloudB_.subscribe(this, "/cloudB", qos.get_rmw_qos_profile());
+
+  pub_ = this->create_publisher<std_msgs::msg::String>("/test/out", 10);
+
+  sync_ = std::make_shared<
+      message_filters::Synchronizer<
+        message_filters::sync_policies::ApproximateTime<
+          sensor_msgs::msg::PointCloud2,
+          sensor_msgs::msg::PointCloud2
+        >
+      >
+    >( message_filters::sync_policies::ApproximateTime<
+         sensor_msgs::msg::PointCloud2,
+         sensor_msgs::msg::PointCloud2>(queue_size),
+      cloudA_, cloudB_ );
+
+  sync_->registerCallback(
+    std::bind(&FusionNode::sync_callback, this, std::placeholders::_1, std::placeholders::_2)
+  );
+  sync_->setAgePenalty(slop);
 }
 
-void FusionNode::on_timer()
+void FusionNode::sync_callback(
+  const sensor_msgs::msg::PointCloud2::ConstSharedPtr &a,
+  const sensor_msgs::msg::PointCloud2::ConstSharedPtr &b) 
 {
-  RCLCPP_DEBUG(get_logger(), "Fusion node tick");
+  RCLCPP_INFO(this->get_logger(), "Synchronized clouds received: A timestamp %u, B timestamp %u", a->header.stamp.sec, b->header.stamp.sec);
+  
+  auto message = std_msgs::msg::String();
+  message.data = "Synced Data!";
+  pub_->publish(message);
+  // do something else
 }
 
 }  // namespace fusion
